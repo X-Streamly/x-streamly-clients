eaa8f127bb2989b27a6c1a4a3d72a69cbc57f96d
Brian Willard <bwillard@cerrio.com>
2012-01-19 13:49:14 -0600

    remove some account specific information

--- java/samples/ly/xstream/simple.java
+++ java/samples/ly/xstream/simple.java
@@ -8,7 +8,7 @@ public class simple {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ResfullClient client = new ResfullClient("10bc1643-c9f5-4210-9814-cae3203af316","bwillard@x-stream.ly","dcba1234");
+		ResfullClient client = new ResfullClient("app key","email address","password");
 		try {
 			Callback c = new Callback();
 			c.channel="C";
@@ -17,30 +17,30 @@ public class simple {
 			
 			client.send("MyChannel","MyEvent",c);
 			
-			/*client.setCallback("MyChannel", "http://asdfasdfasdf.com", "Don't tell anyone", "bob");
+			client.setCallback("MyChannel", "http://asdfasdfasdf.com", "Don't tell anyone", "bob");
 			for(Callback callback : client.getCallbacks().items){
 				System.out.println(callback);
 				client.removeCallback(callback.key);
-			}*/
+			}
 			
-			/*Channels channels = client.activeChannels();
+			Channels channels = client.activeChannels();
 			for(String key : channels.channels.keySet()){
 				System.out.println("Channel "+key+", "+channels.channels.get(key));
-			}*/
+			}
 			
-			//System.out.println(client.usageConnections());
+			System.out.println(client.usageConnections());
 			
-			//System.out.println(client.usageConnections());
+			System.out.println(client.usageConnections());
 			
-			//String token = client.createToken(true, false, null, "Event", null, false);
+			String token = client.createToken(true, false, null, "Event", null, false);
 			
-			//System.out.println(token);
+			System.out.println(token);
 			
-			//client.deleteToken(token);
+			client.deleteToken(token);
 			
-			/*for(Token t:client.getTokens().sessions){
+			for(Token t:client.getTokens().sessions){
 				System.out.println(t.key);
-			}*/
+			}
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
