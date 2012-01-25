f0ce6090bc5ccd5b84004191bd4cc48211586046
Brian Willard <bwillard@gmail.com>
2012-01-25 10:32:09 -0600

    add a PHP version of the client library

---
+++ php/examples/example.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+require_once '../src/xstreamly.php';
+
+$xstreamlyClient = new XStreamly\XstreamlyClient('10bc1643-c9f5-4210-9814-cae3203af316','bwillard@x-stream.ly','foobar123');
+
+
+//mesage sending
+$xstreamlyClient->send('MyChannel','MyEvent',"some data",false);
+
+$xstreamlyClient->send('MyChannel','MyEvent',array("xstreamly"=> "is cool","this"=>"is fun"),false);
+
+
+
+//registering callbacks
+$callBackKey = $xstreamlyClient->setCallback('MyChannel','http://my.endponit','secret','event');
+echo "callback key: " . $callBackKey . "\n";
+$callbacks = $xstreamlyClient->getCallbacks();
+$firstcallback = $callbacks[0]; 
+echo "callback: " . json_encode($firstcallback) . "\n";
+
+$xstreamlyClient->removeCallback($callBackKey);
+
+
+//usage data
+echo "active channels: " . json_encode($xstreamlyClient->getActiveChannels()) ."\n";
+
+echo "message usage: " . json_encode($xstreamlyClient->getMessageUsage()) ."\n";
+
+echo "connection usage: " . json_encode($xstreamlyClient->getConnectionUsage()) ."\n";
+
+
+
+//managing security tokens
+$securityToken = $xstreamlyClient->createToken(true,true,null,"MyEvent",null,false);
+echo "security token key: " . $securityToken . "\n";
+
+echo "callback: " . json_encode($xstreamlyClient->getTokens()) . "\n";
+
+$xstreamlyClient->deleteToken($securityToken);
+
+?>
